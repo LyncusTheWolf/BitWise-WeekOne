83de0b22a404355df6e833668ab0a7832f250e8b
LyncusTheWolf <Kenneth_anime@yahoo.com>
2021-08-12 20:32:38 -0700

    Component Test
    Created a sub component for testing purposes

--- react_omdb/src/utils.js
+++ react_omdb/src/utils.js
@@ -1,16 +1,32 @@
 //Key is nulled out to avoid commiting private data
-let keyRaw = "--------";
+//const apiKey = "ffe01f6a";
+const apiString = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
-getMoviesBySearchTerm = async (searchTerm) =>{
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${keyRaw}&t=${searchTerm}`);
+/*
+export const getMoviesBySearchTerm = async (searchTerm) =>
+    await fetch(apiString)
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => err);
+
+export const getMovieDetailsById = async (movieId="tt3896198") =>
+    await fetch(apiString)
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => err);
+*/
+
+export const getMoviesBySearchTerm = async (searchTerm) =>{
+    const response = await fetch(`${apiString}&s=${searchTerm}`);
     let movieJSON = await response.json();
 
     return movieJSON;
 }
 
-getMovieDetailsById = async (searchID) =>{
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${keyRaw}&i=${searchID}`);
+export const getMovieDetailsById = async (searchID="tt3896198") =>{
+    const response = await fetch(`${apiString}&i=${searchID}`);
     let movieJSON = await response.json();
 
     return movieJSON;
 }
+
